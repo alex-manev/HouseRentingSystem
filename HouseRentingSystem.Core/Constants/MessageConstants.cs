2834ffc703cb837715009c7a4ce785c440f51541
alex-manev <alkesmanev2005@gmail.com>
2024-03-13 21:59:15 +0200

    become an agent

--- HouseRentingSystem.Core/Constants/MessageConstants.cs
+++ HouseRentingSystem.Core/Constants/MessageConstants.cs
@@ -6,5 +6,11 @@
 
         public const string LengthMessage = "The {0} field must be between {2} and {1} characters long";
 
+        public const string PhoneExists = "Phone number already exists. Enter another one";
+
+        public const string HasRents = "You should have no rents to become an agent";
+
+
+
     }
 }
